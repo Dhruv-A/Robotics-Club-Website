475a2446c45233965c70766624234fd0ecd418ec
halcyonhunter <timmyme2@gmail.com>
2019-09-12 03:18:30 +1000

    copy pasted some code for login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -20,6 +20,18 @@ class Login extends Component {
         <Header />
         <div class="login_section login_bg">
           <div class="login_box">
+            <h class="login_title"><b>Login</b></h>
+
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required></input>
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required></input>
+
+            <button type="submit">Login</button>
+            <label>
+              <input type="checkbox" checked="checked" name="remember"> Remember me</input>
+            </label>
           </div>
         </div>
       </div>
